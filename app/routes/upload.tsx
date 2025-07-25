ca35c99ff4231c606278ef0d32d11651493867b4
Kartik Verma <vermakartik667@gmail.com>
2025-07-25 15:39:34 +0530

    creater analyse functionality, created cloud storage and obtained resume info from llm model and ai

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,11 +1,18 @@
+import { prepareInstructions } from 'constants'
 import React, { useState, type FormEvent } from 'react'
+import { useNavigate } from 'react-router'
 import FileUploader from '~/components/FileUploader'
 import Navbar from '~/components/Navbar'
+import { convertPdfToImage } from '~/lib/pdf2img'
+import { usePuterStore } from '~/lib/puter'
+import { generateUUID } from '~/lib/utils'
 
 function upload() {
+    const {fs, kv,auth, isLoading, ai }=usePuterStore()
     const [isProcessing, setisProcessing] = useState(false)
     const [status, setstatus] = useState("")
     const [file, setfile] = useState<File | null>(null)
+    const navigate=useNavigate()
 
     const handleFileSelect=(file : File | null)=>{
         setfile(file)
@@ -18,14 +25,97 @@ function upload() {
         const form=document.querySelector("#form")
         if(!form) return
         const formData=new FormData(form)
-        const companyName=formData.get("company-name")
-        const jobDesc=formData.get("job-title")
-        const jobTitle=formData.get("job-desc")
+        const companyName=formData.get("company-name") as string
+        const jobDesc=formData.get("job-title") as string
+        const jobTitle=formData.get("job-desc") as string
 
         console.log({
             file,companyName,jobDesc,jobTitle
         });
+
+        const res=handleAnalyse({companyName,jobDesc,jobTitle})
+            
+
+
+
         
+        
+
+
+    }
+
+    async function handleAnalyse({companyName,jobDesc,jobTitle}:{
+        companyName:string,jobDesc:string, jobTitle:string
+    }){
+        if(!file){
+            setstatus("No file selected")
+            setisProcessing(false)
+            return;
+        }
+
+        setisProcessing(true)
+        setstatus("Uploading the file...")
+
+        const uploadedFile=await fs.upload([file])
+
+        if(!uploadedFile){
+            setstatus("Error: Failed to upload file")
+        }
+
+        setstatus("Converting to image ")
+        const imageFile=await convertPdfToImage(file)
+        
+        if(!imageFile){
+            setstatus("Failed to convert PDF to image")
+        } 
+        
+        
+        setstatus("Uploading the image ")
+        const uploadedImage=await fs.upload([imageFile.file])
+        
+
+        if(!uploadedImage) setstatus("Failed to upload image")
+
+        setstatus("Preparing the data")
+
+        const uuid=generateUUID()
+        
+
+        //now data
+        const data={
+            uuid,
+            resumePath:uploadedFile.path,
+            imagePath:uploadedImage.path,
+            companyName,jobDesc,jobTitle,
+            feedback:""
+        }
+
+        await kv.set(`resume:${uuid}`,JSON.stringify(data)) 
+
+        setstatus("Analysing...")
+
+        const feedback=await ai.feedback(uploadedFile?.path,
+            prepareInstructions({jobTitle, jobDesc})
+        )
+
+        if(!feedback) setstatus("Failed to analyse resume")
+
+        const feedbackText=typeof feedback?.message.content === "string" ? feedback?.message.content :
+        feedback?.message.content[0].text
+
+        data.feedback=JSON.parse(feedbackText)
+
+        await kv.set(`resume:${uuid}`,JSON.stringify(data))
+
+        setstatus("Analysis complete, redirecting...")
+        console.log(data);
+        
+
+
+
+
+
+
 
 
     }
